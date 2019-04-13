25deacee6d15d611d3998133f4ab99b89890909a
Decker_MMIV <decker_mmiv@hotmail.com>
2019-04-13 11:45:04 +0200

    - Some minor corrections

--- QuickCamera/QuickFeet.lua
+++ QuickCamera/QuickFeet.lua
@@ -26,8 +26,8 @@ end
 
 Player.QC_onInputLookLeftRight = function(self, inputActionName, inputValue, callbackState, arg4, isMouse)
   --log("Player.QC_onInputLookLeftRight: ",inputActionName," ",inputValue," ",callbackState," ",arg4," ",isMouse)
-  if inputValue == 0 then
-    if self.inputInformation.modQcPressedTime ~= nil then
+  if 0 == inputValue then
+    if nil ~= self.inputInformation.modQcPressedTime then
       if (g_time - self.inputInformation.modQcPressedTime) <= 150 then -- TODO: Make 150ms configurable!
         self.inputInformation.yawCamera = ((math.pi / 4) * self.inputInformation.modQcInputValue) / g_gameSettings:getValue(GameSettings.SETTING.CAMERA_SENSITIVITY)
       end
@@ -40,11 +40,11 @@ Player.QC_onInputLookLeftRight = function(self, inputActionName, inputValue, cal
 end
 
 Player.load = Utils.appendedFunction(Player.load, function(self, xmlFilename, playerStyle, creatorConnection, isOwner)
-  if InputAction.QuickCamOnFootForeBack ~= nil then
+  if nil ~= InputAction.QuickCamOnFootForeBack then
     --log("exists: InputAction.QuickCamOnFootForeBack")
     self.inputInformation.registrationList[InputAction.QuickCamOnFootForeBack] = { eventId="", callback=self.QC_onInputLookForeBack, triggerUp=false, triggerDown=true, triggerAlways=false, activeType=Player.INPUT_ACTIVE_TYPE.IS_MOVEMENT, callbackState=nil, text="", textVisibility=false }
   end
-  if InputAction.QuickCamOnFootLeftRight ~= nil then
+  if nil ~= InputAction.QuickCamOnFootLeftRight then
     --log("exists: InputAction.QuickCamOnFootLeftRight")
     self.inputInformation.registrationList[InputAction.QuickCamOnFootLeftRight] = { eventId="", callback=self.QC_onInputLookLeftRight, triggerUp=true, triggerDown=true, triggerAlways=false, activeType=Player.INPUT_ACTIVE_TYPE.IS_MOVEMENT, callbackState=nil, text="", textVisibility=false }
   end
@@ -64,21 +64,25 @@ end)
 
 Player.mouseEvent = Utils.appendedFunction(Player.mouseEvent, function(self, posX, posY, isDown, isUp, button)
   if isDown then
-    if button == Input.MOUSE_BUTTON_WHEEL_UP then
+    if button == Input.MOUSE_BUTTON_WHEEL_UP and g_currentMission:getHasPlayerPermission("manageRights") then
       self.inputInformation.modQcRunSpeedModifier = 1.25
-    elseif button == Input.MOUSE_BUTTON_WHEEL_DOWN then
+    elseif button == Input.MOUSE_BUTTON_WHEEL_DOWN and g_currentMission:getHasPlayerPermission("manageRights") then
       self.inputInformation.modQcRunSpeedModifier = 0.9
     end
   end
 end);
 
 Player.updatePlayerStates = Utils.appendedFunction(Player.updatePlayerStates, function(self)
-  if self.inputInformation.modQcRunSpeedModifier ~= nil then
+  if nil ~= self.inputInformation.modQcRunSpeedModifier then
     self.motionInformation.maxRunningSpeed = math.max(1, math.min(128, self.motionInformation.maxRunningSpeed * self.inputInformation.modQcRunSpeedModifier))
     self.inputInformation.modQcRunSpeedModifier = nil
-  elseif self.inputInformation.runAxis <= 0 then
+    self.inputInformation.modQcRunSpeedModified = true
+  elseif self.inputInformation.runAxis <= 0 and true == self.inputInformation.modQcRunSpeedModified then
     self.motionInformation.maxRunningSpeed = 9 -- The constant '9' seems to be the default value.
+    self.inputInformation.modQcRunSpeedModified = nil
   end
 end)
+
+----
 
 print(("Script loaded: QuickFeet.lua - from %s (v%s)"):format(g_currentModName, g_modManager:getModByName(g_currentModName).version));
