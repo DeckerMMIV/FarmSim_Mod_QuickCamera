4df26621642371d12fd85f7bb289f201c6437b82
Decker_MMIV <decker_mmiv@hotmail.com>
2024-11-13 16:19:00 +0100

    - Made 'Quick Feet' available to all players in multiplayer - 'Quick Feet' work-around due to LumberJack mod also modifying player's running speed

--- QuickCamera/src/QuickCameraPlayer.lua
+++ QuickCamera/src/QuickCameraPlayer.lua
@@ -64,24 +64,44 @@ end)
 
 Player.mouseEvent = Utils.appendedFunction(Player.mouseEvent, function(self, posX, posY, isDown, isUp, button)
   if isDown then
-    if button == Input.MOUSE_BUTTON_WHEEL_UP and g_currentMission:getHasPlayerPermission("manageRights") then
+    if button == Input.MOUSE_BUTTON_WHEEL_UP then
       self.inputInformation.modQcRunSpeedModifier = 1.25
-    elseif button == Input.MOUSE_BUTTON_WHEEL_DOWN and g_currentMission:getHasPlayerPermission("manageRights") then
+    elseif button == Input.MOUSE_BUTTON_WHEEL_DOWN then
       self.inputInformation.modQcRunSpeedModifier = 0.9
     end
   end
 end);
 
-local basegameRunningSpeed = 9 -- The constant '9' seems to be the default value.
+local basegameRunningSpeed =  9 -- The constant '9' seems to be the default value.
 
 Player.updatePlayerStates = Utils.appendedFunction(Player.updatePlayerStates, function(self)
   if nil ~= self.inputInformation.modQcRunSpeedModifier then
-    self.motionInformation.maxRunningSpeed = math.max(1, math.min(QuickCamera.playerRunSpeedFactorMax * basegameRunningSpeed, self.motionInformation.maxRunningSpeed * self.inputInformation.modQcRunSpeedModifier))
+    self.motionInformation.maxRunningSpeed      = math.max(1, math.min(QuickCamera.playerRunSpeedFactorMax * basegameRunningSpeed, self.motionInformation.maxRunningSpeed * self.inputInformation.modQcRunSpeedModifier))
+    self.motionInformation.maxCheatRunningSpeed = self.motionInformation.maxRunningSpeed
     self.inputInformation.modQcRunSpeedModifier = nil
     self.inputInformation.modQcRunSpeedModified = true
+    -- Due to the LumberJack mod is interfering with QuickCamera's implementation of setting player speed,
+    -- so a workaround is needed.
+    if  nil ~= FS22_LumberJack
+    and nil ~= FS22_LumberJack.LumberJack
+    and nil ~= FS22_LumberJack.LumberJack.originalRunningSpeed
+    then
+      FS22_LumberJack.LumberJack.originalRunningSpeed = self.motionInformation.maxRunningSpeed
+      FS22_LumberJack.LumberJack.maxRunningSpeed      = self.motionInformation.maxRunningSpeed
+    end
   elseif self.inputInformation.runAxis <= 0 and true == self.inputInformation.modQcRunSpeedModified then
-    self.motionInformation.maxRunningSpeed = basegameRunningSpeed
+    self.motionInformation.maxRunningSpeed      = basegameRunningSpeed
+    self.motionInformation.maxCheatRunningSpeed = basegameRunningSpeed
     self.inputInformation.modQcRunSpeedModified = nil
+    -- Due to the LumberJack mod is interfering with QuickCamera's implementation of setting player speed,
+    -- so a workaround is needed.
+    if  nil ~= FS22_LumberJack
+    and nil ~= FS22_LumberJack.LumberJack
+    and nil ~= FS22_LumberJack.LumberJack.originalRunningSpeed
+    then
+      FS22_LumberJack.LumberJack.originalRunningSpeed = self.motionInformation.maxRunningSpeed
+      FS22_LumberJack.LumberJack.maxRunningSpeed      = self.motionInformation.maxRunningSpeed
+    end
   end
 end)
 
@@ -89,8 +109,10 @@ local infoText_SpeedRun = g_i18n:getText("QUICKCAM_ONFOOT_SPEEDRUN")
 
 Player.update = Utils.appendedFunction(Player.update, function(self, dt)
   if self.isClient and self.isControlled then
-    if self.inputInformation.runAxis > 0 and not self:hasHandtoolEquipped() then
-      if g_currentMission:getHasPlayerPermission("manageRights") then
+    if self.inputInformation.runAxis > 0 then
+      if not self:hasHandtoolEquipped()
+      or (nil ~= FS22_LumberJack and nil ~= FS22_LumberJack.LumberJack)
+      then
         g_currentMission:addExtraPrintText((infoText_SpeedRun):format(self.motionInformation.maxRunningSpeed / basegameRunningSpeed))
       end
     end
